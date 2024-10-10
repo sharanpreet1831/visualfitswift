81f3b2f042fcc64a36ab3b8cd773ea63bcfb724b
sharanpreet singh <sharanpreet1831.be21@chitkara.edu.in>
2024-10-10 11:45:23 +0530

    first message

---
+++ VisualFitNEW/VisualFitNEWApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualFitNEWApp.swift
+//  VisualFitNEW
+//
+//  Created by Sharanpreet Singh  on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct VisualFitNEWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisualFitNEWTests/VisualFitNEWTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisualFitNEWTests.swift
+//  VisualFitNEWTests
+//
+//  Created by Sharanpreet Singh  on 10/10/24.
+//
+
+import Testing
+@testable import VisualFitNEW
+
+struct VisualFitNEWTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
